baa295bf1ccf905daa7718f2a0a014bbadec3b01
elvmariga <elvmariga@gmail.com>
2023-05-04 16:19:32 +0300

    first commit

---
+++ FirstJava.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+// import java.util.Scanner;
+
+public class FirstJava {
+
+        public static void main(String[] args) {
+                
+                
+                for ( int i = 0; i<10; i++)
+                System.out.println("Hello " + i );
+                
+        
+        }
+
+}
