0ee9f8572a8e21a58955ed0fe23470f67328c234
elvmariga <elvmariga@gmail.com>
2023-05-04 16:31:49 +0300

    Added a new class Calculator

--- FirstJava.java
+++ FirstJava.java
@@ -1,15 +1,26 @@
-import java.util.Scanner;
 
-// import java.util.Scanner;
+class Calculator{
+        
+        public int perimeter(int w, int l){
+                int p =w*l;
+                return p;
+        }
+}
 
 public class FirstJava {
 
         public static void main(String[] args) {
                 
                 
-                for ( int i = 0; i<10; i++)
-                System.out.println("Hello " + i );
+                // for ( int i = 0; i<10; i++)
+                // System.out.println("Hello " + i );
                 
+                Calculator rect = new Calculator();
+
+                int results = rect.perimeter(4, 9);
+
+
+                System.out.println(results);
         
         }
 
